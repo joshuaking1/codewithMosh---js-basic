4c43e0f0e65db02bf53ea0bafe3e8b2ce6e87c46
joshuaking1 <167214974+joshuaking1@users.noreply.github.com>
2025-01-29 01:52:43 +0000

    Add showNumbers function to display even and odd numbers up to a limit

--- 14- Exercise 5- Even and Odd Numbers.js
+++ 14- Exercise 5- Even and Odd Numbers.js
@@ -2,8 +2,7 @@ showNumbers(10);
 
 function showNumbers(limit) {
     for (let i = 0; i <= limit; i++) {
-        const message = (i % 2 === 0) ? "EVEN" : "ODD";
-        console.log(i, message);
+        const message = (i % 2 === 0) ? "EVEN" : "OOD";
+        console.log(i, message)
     }
-
 }
